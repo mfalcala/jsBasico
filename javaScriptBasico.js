1e24162c2d18e82884ce1747538cdee921b9bbbe
Miguel Alcala <89943110+mfalcala@users.noreply.github.com>
2022-11-28 20:54:15 -0300

    Add files via upload
    1-40

---
+++ javaScriptBasico.js
@@ -0,0 +1,233 @@
+//1 Comment your js code
+//Comentario en una linea
+
+/* Comentario en mas de una linea
+*/
+
+//2 Declare JavaScript Variables
+var myName;
+
+//3 Storing Values with the Assignment Operator
+// Setup
+var a;
+
+// Only change code below this line
+a = 7;
+
+//4 Assigning the Value of One Variable to Another
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a;
+
+//5 Initializing Variables with the Assignment Operator
+var a = 9;
+
+//6 Declare String Variables
+var myFirstName = "Miguel";
+var myLastName = "Alcala";
+
+//7 Understanding Uninitialized Variables
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//8 Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//9 Explore Differences Between the var and let Keywords
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//10 Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//11 Add Two Numbers with JavaScript
+const sum = 10 + 10;
+//console.log(sum);
+
+//12 Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+
+//13 Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+
+//14 Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+
+//15 Increment a Number with JavaScript
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//16 Decrement a Number with JavaScript
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+//17 Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+
+// Only change code below this line
+let myDecimal = 5.7;
+
+//18 Multiply Two Decimals with JavaScript
+const product = 2.0 * 2.5;
+
+//19 Divide One Decimal by Another with JavaScript
+const quotient = 4.4 / 2.0; // Change this line
+
+//20 Finding a Remainder in JavaScript
+const remainder = 11 % 3;
+
+//21 Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//22 Compound Assignment With Augmented Subtraction
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+//23 Compound Assignment With Augmented Multiplication
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+//24 Compound Assignment With Augmented Division
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//25 Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//26 Quoting Strings with Single Quotes
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//27 Escape Sequences in Strings
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//28 Concatenating Strings with Plus Operator
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+//29 Concatenating Strings with the Plus Equals Operator
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+//30 Constructing Strings with Variables
+// Only change code below this line
+const myName = "Miguel";
+const myStr = "My name is " + myName + "and I am well";
+
+//31 Appending Variables to Strings
+// Change code below this line
+const someAdjective = "nice";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+//32 Find the Length of a String 
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+//33 Use Bracket Notation to Find the First Character in a String
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//34 Understand String Immutability
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+//35 Use Bracket Notation to Find the Nth Character in a String
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+//36 Use Bracket Notation to Find the Last Character in a String
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+//37 Use Bracket Notation to Find the Nth-to-Last Character in a String
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+//38 Word Blanks
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb+ " " + myAdverb + " to catch de ball"; // Change this line
+// Only change code above this line
+
+//39  Store Multiple Values in one Variable using JavaScript Arrays
+// Only change code below this line
+const myArray = ["22", 22];
+
+//40 Nest one Array within Another Array
+// Only change code below this line
+const myArray = [[], []];
